437bff450554102cba4206167ba9c7978760aa10
Andres Espinoza <a_espinosa@live.com>
2017-04-18 02:16:17 -0400

    minus pblish

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
 var wrench = require('wrench');
 
 /**
@@ -27,11 +26,4 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
  */
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
-});
-
-
-// Deploy to gh-pages
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
 });
